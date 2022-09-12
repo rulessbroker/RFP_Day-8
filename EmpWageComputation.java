a9f158b84f95aee8b99e0136c500dde2a2caf86a
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-12 13:36:08 +0530

    Welcome Massage

---
+++ EmpWageComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.empwage;
+
+public class EmpWageComputation {
+	
+public static void main(String[] args) {
+		
+		System.out.println("Welcome To Employee Wage Computation Program");
+		
+	}
+}
