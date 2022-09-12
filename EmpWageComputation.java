7aa6e3ac303df181e515a930e886490891b324da
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-12 13:51:16 +0530

    Amended code

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -8,21 +8,26 @@ public static void main(String[] args) {
 		
 		System.out.println("Welcome To Employee Wage Computation Program");
 		
-		Random rand = new Random();
-	    int random = 3;
-		int randNum = rand.nextInt(random);
+		CheckEmployee();
+}
+
+public static int CheckEmployee() {
+	Random rand = new Random();
+    int random = 3;
+	int randNum = rand.nextInt(random);
+	
+	switch(randNum) {
+	case 1:
+		System.out.println("Employee Is Present");
+		break;
 		
-		switch(randNum) {
-		case 1:
-			System.out.println("Employee Is Present");
-			break;
-			
-		case 2: 
-			System.out.println("Employee is present Part time");
-			break;
-			
-		default : 
-			System.out.println("Employee is Absent");
-		}
+	case 2: 
+		System.out.println("Employee is present Part time");
+		break;
+		
+	default : 
+		System.out.println("Employee is Absent");
+	}
+	return randNum;
 	}
 }
