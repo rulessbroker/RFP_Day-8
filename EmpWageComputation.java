e9c26622930d7e0da9f3ef3e40625329d4ca8f7a
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-12 13:59:44 +0530

    Employee part time wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,42 +6,51 @@ public class EmpWageComputation {
 	
 	static int wagePerHr = 20;
 	static int fullDayHr = 8;
+	static int partTimeHr = 4;
 	
-public static void main(String[] args) {
-	
-		System.out.println("Welcome To Employee Wage Computation Program");
+	public static void main(String[] args) {
 		
-		CheckEmployee();
-		
-		DailyWage();
-	}
+			System.out.println("Welcome To Employee Wage Computation Program");
+			
+			CheckEmployee();
+			
+			DailyWage();
+			
+			PartTimeWage();
+		}
 
 //Employee Wage
 
-public static int DailyWage() {
-	int dailyWage = wagePerHr * fullDayHr;
-	System.out.println("Employee Daily Wage is = " +dailyWage);
-	return (dailyWage);
-	}
+	public static int DailyWage() {
+		int dailyWage = wagePerHr * fullDayHr;
+		System.out.println("Employee Daily Wage is = " +dailyWage);
+		return (dailyWage);
+		}
 
 
-public static int CheckEmployee() {
-	Random rand = new Random();
-    int random = 3;
-	int randNum = rand.nextInt(random);
-	
-	switch(randNum) {
-	case 1:
-		System.out.println("Employee Is Present");
-		break;
+	public static int CheckEmployee() {
+		Random rand = new Random();
+	    int random = 3;
+		int randNum = rand.nextInt(random);
 		
-	case 2: 
-		System.out.println("Employee is present Part time");
-		break;
-		
-	default : 
-		System.out.println("Employee is Absent");
-	}
-	return randNum;
-	}
+		switch(randNum) {
+		case 1:
+			System.out.println("Employee Is Present");
+			break;
+			
+		case 2: 
+			System.out.println("Employee is present Part time");
+			break;
+			
+		default : 
+			System.out.println("Employee is Absent");
+		}
+		return randNum;
+		}
+
+	public static int PartTimeWage() {
+		int partTime = wagePerHr * partTimeHr;
+		System.out.println("Employee Part Time Wage is = " +partTime);
+		return (partTime);
+		}
 }
